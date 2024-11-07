c071b965cbf0672612d58d6946b7dde712eecd17
igoeco <wegoeco@gmail.com>
2024-11-07 12:48:57 -0600

    update to new lc3

--- Arrays/FindMax/FindMax-Array.asm
+++ Arrays/FindMax/FindMax-Array.asm
@@ -34,3 +34,21 @@ DoneLooking
     TRAP  x25
 stuarrayloc  .FILL    x4500
     .END 
+
+; Array of student records with each student record
+; made of two attributes: Name and Score
+; Name is always only two characters and is null-terminated
+        .ORIG   x4500
+stuarray
+        .STRINGZ "AZ"
+        .FILL #75
+        .STRINGZ "BY"
+        .FILL #85
+        .STRINGZ "CX"
+        .FILL #62
+        .STRINGZ "DW"
+        .FILL #94
+        .STRINGZ "EU"
+        .FILL #72
+        .FILL #0   ; Sentinel is a zero
+        .END	
